b0e6bf6d0da5cdfb3ebdde544a50199c27305a22
ComSciBR <147848208+ComSciBR@users.noreply.github.com>
2025-05-06 10:00:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("homeButton").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+document.getElementById("threeDbutton").addEventListener("click", function() {
+    window.location.href = "3dprinting.html";
+});
+
+document.getElementById("electroButton").addEventListener("click", function() {
+    window.location.href = "electroassembly.html";
+});
